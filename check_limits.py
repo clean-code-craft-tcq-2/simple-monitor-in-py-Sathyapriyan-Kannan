7e19f9ba266564c94632f1d2c5cc0503e3e23004
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-02-08 11:04:39 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -13,18 +13,21 @@ class LimitChecker:
         return True
 
 
-def battery_is_ok(temperature, soc, charge_rate):
+def check_limits(temp, soc, charge_rate):
+    if temp.checkLimit() and soc.checkLimit() and charge_rate.checkLimit():
+        return True
 
+
+def battery_is_ok(temperature, soc, charge_rate):
     temp_check = LimitChecker(temperature, PermissibleLimit.temperature_limit)
     soc_check = LimitChecker(soc, PermissibleLimit.soc_limit)
     charge_rate_check = LimitChecker(charge_rate, PermissibleLimit.charge_rate_limit)
 
-    if temp_check.checkLimit() and soc_check.checkLimit() and charge_rate_check.checkLimit():
+    if check_limits(temp_check, soc_check, charge_rate_check) is True:
         return True
     return False
 
 
 if __name__ == '__main__':
-
     assert (battery_is_ok(25, 70, 0.7) is True)
     assert (battery_is_ok(50, 85, 0) is False)
