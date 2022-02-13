d272d8a8395eb7fcbf7f7d0cf88965e6bde7b358
Sathyapriyan Kannan <sathyapriyankannan@gmail.com>
2022-02-13 23:47:27 +0530

    Update BMS Management

--- BMSManagement.py
+++ BMSManagement.py
@@ -1,10 +1,17 @@
 class BMSManagement:
-    def __init__(self, min_threshold, max_threshold, factor):
+    def __init__(self, min_threshold, max_threshold, factor, issue_warning, warning_tolerance_rate=5):
         self.min_threshold = min_threshold
         self.max_threshold = max_threshold
         self.factor = factor
+        self.issue_warning = issue_warning
+
+        if issue_warning:
+
+            warning_tolerance = self.max_threshold * (warning_tolerance_rate / 100)
+            self.min_warn_tolerance = self.min_threshold + warning_tolerance
+            self.max_warn_tolerance = self.max_threshold - warning_tolerance
 
 
-temperature_limit = BMSManagement(0, 45, "Temperature")
-soc_limit = BMSManagement(20, 80, "State of Charge")
-charge_rate_limit = BMSManagement(0, 0.8, "Charge Rate")
+temperature_limit = BMSManagement(0, 45, "Temperature", True)
+soc_limit = BMSManagement(20, 80, "State of Charge", True)
+charge_rate_limit = BMSManagement(0, 0.8, "Charge Rate", True)
